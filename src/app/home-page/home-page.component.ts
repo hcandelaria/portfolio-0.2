c0fcc4866ae9aea9b32e39e327e9dc27cf7ec83d
hcandelaria <hcandelaria66.7@gmail.com>
2018-10-19 15:07:32 -0400

    added new features to the home page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -6,10 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-
-  constructor() { }
+  
+  constructor() { 
+  }
+  currentWord = "WELCOME"
+  welcomeWord = [
+    'WELCOME',
+    'BIENVENIDO',
+    'BENVENUTO',
+    'BIENVENUE',
+    'BEM VINDA',
+    'WITAMY',
+    'HERZLICH WILLKOMMEN',
+    'ようこそ',
+    '欢迎',
+    'أهلا بك',
+  ];
+  
+  welcomeWordWillUpdate = () => {
+    this.pickWelcomeWord(this.handleTimeOut);
+  }
+  
+  pickWelcomeWord = (cb) => {
+    let randomNumber = Math.floor(Math.random() * this.welcomeWord.length);
+    this.currentWord = this.welcomeWord[randomNumber]
+    return cb();
+  }
+  handleTimeOut = () =>{
+    try{
+      setTimeout(this.welcomeWordWillUpdate, 3000);
+    }catch(e) {
+      console.log('TimeOut error:', e);
+    }
+  }
 
   ngOnInit() {
+    setTimeout(this.welcomeWordWillUpdate, 3000);
   }
 
 }
